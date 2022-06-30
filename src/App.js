7cd86c9e49c36146086001cf4776004485f86e1a
fatemeh6889 <fatemeh.qasemkhani@external.iherb.com>
2022-07-01 00:18:00 +0300

    added commemnts

--- src/App.js
+++ src/App.js
@@ -3,79 +3,94 @@ import './App.css';
 
 import { addWatchedMovie, add, removeWatchedMovie, getWatchedMovies, getAllMovies } from './index.js';
 
-const getMoviesComponents = (movies) => {
-  var components = [];
+// Define same pattern for function definition (function keyword or const?)
 
-  movies.forEach(function(movie) {
-    components.push(
-      <div className="all">
-        <div>
-          <img src={movie.image} height="100px" />
-        </div>
-        <span>
+const getMoviesComponents = (movies) => {
+	// Avoid using var for defining variables, alternative keywords (let, const)
+	var components = [];
+
+	// This should be converted to a separate file as component that takes a single movie as prop. Then we probably don't even need for function as we can just do
+	// Better to remove function keyword and use shortest syntax in both part (for forEach and onClick)
+	// Use Map() method for iterating, Its faster and creates new array so you dont need to define an empty array and push the objects to it
+	// Remove extra HTML elements
+	// For A tag, add e.preventDefault method to prevent the link from following the url or jump to the top of the page
+	// Better not to add inline styles
+	movies.forEach(function(movie) {
+		components.push(
+			<div className="all">
+				<div>
+					<img src={movie.image} height="100px" />
+				</div>
+				<span>
           <a className="movie-watched" href="#" onClick={function() { addWatchedMovie(movie.title, movie.comment, movie.image) }}>
             {movie.title}
           </a>
         </span>
-        <br />
-        <span>
+				<br />
+				<span>
           {movie.comment}
         </span>
-        <br />
-        <br />
-      </div>
-    )
-  })
+				<br />
+				<br />
+			</div>
+		)
+	})
 
-  return components;
+	return components;
 }
 
 function getWatchedMoviesComponents(movies) {
-  var components = [];
-
-  movies.forEach(function(movie) {
-    components.push(movie && (
-      <div className="watched">
-        <div>
-          <img src={movie.image} height="100px" />
-        </div>
-        <span>
+	var components = [];
+	// Same comments like getMoviesComponents function
+	movies.forEach(function(movie) {
+		components.push(movie && (
+			<div className="watched">
+				<div>
+					<img src={movie.image} height="100px" />
+				</div>
+				<span>
           <a className="movie-watched" href="#" onClick={function() { removeWatchedMovie(movie.title) }}>
             {movie.title}
           </a>
         </span>
-        <br />
-        <span>
+				<br />
+				<span>
           {movie.comment}
         </span>
-        <br />
-        <br />
-      </div>
-    ))
-  })
+				<br />
+				<br />
+			</div>
+		))
+	})
 
-  return components;
+	return components;
 }
 
+// Add a type checker to the project like prop-types, flow or typescript, there is not validation for data
+// Create Form component for Add movie, later on you most probably need to edit the movies which have added
+// Use react hooks for not losing the react life cycle (when changing the input values, and also for the times that UI will update by new entities)
+// Better to use Button tag, not input with button type
+// Why wrapping the input element in <b></b> tag?
 function App(props) {
-  return (
-    <div className="App">
-      <h1>Codest Movies!</h1>
-      <h1>Add movie!</h1>
-      <b>TITLE:<br /><input type="text" onChange={function(e) { title = e.target.value; }} /></b><br />
-      <b>IMAGE URL:<br /><input type="text" onChange={function(e) { image = e.target.value; }} /></b><br />
-      <b>COMMENT:<br /><input type="text" onChange={function(e) { comment = e.target.value; }} /></b><br />
-      <input type="button" onClick={function(e) { add(title, image, comment); }} value="ADD MOVIE" />
+	return (
+		<div className="App">
+			<h1>Codest Movies!</h1>
+			<h1>Add movie!</h1>
+			<b>TITLE:<br /><input type="text" onChange={function(e) { title = e.target.value; }} /></b><br />
+			<b>IMAGE URL:<br /><input type="text" onChange={function(e) { image = e.target.value; }} /></b><br />
+			<b>COMMENT:<br /><input type="text" onChange={function(e) { comment = e.target.value; }} /></b><br />
+			<input type="button" onClick={function(e) { add(title, image, comment); }} value="ADD MOVIE" />
 
-      <h1>Watchlist:</h1>
-      {getMoviesComponents(getAllMovies())}
+			<h1>Watchlist:</h1>
+			{getMoviesComponents(getAllMovies())}
 
-      <h1>Already watched:</h1>
-      {getWatchedMoviesComponents(getWatchedMovies())}
-    </div>
-  );
+			<h1>Already watched:</h1>
+			{getWatchedMoviesComponents(getWatchedMovies())}
+		</div>
+	);
 }
 
+// Strange way and place for define the variables
 var title = '';
 var image = '';
 var comment = '';
--- src/index.js
+++ src/index.js
@@ -3,87 +3,111 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// A lot of these "export functions" would be better in different file or folder. This will improve organization and make it easier to read
 export function getWatchedMovies() {
-	var movies = localStorage.getItem('movies-watched');
+    // Avoid using var for defining variables, alternative keywords (let, const)
+    // If you have keys in localStorage and using them in different part of codes, that would be better to define variable for them
+    var movies = localStorage.getItem('movies-watched');
 
-	if (!movies) {
-		return [];
-	} else {
-		return JSON.parse(movies);
-	}
+    // Use ternary operator, simpler and shorter
+    if (!movies) {
+        return [];
+    } else {
+        return JSON.parse(movies);
+    }
 }
 
 export function getAllMovies() {
-	var movies = localStorage.getItem('movies-all');
+    // Avoid using var for defining variables, alternative keywords (let, const)
+    // If you have keys in localStorage and using them in different part of codes, that would be better to define variable for them
+    var movies = localStorage.getItem('movies-all');
 
-	if (!movies) {
-		return [
-		{
-			title: 'The Avengers',
-			image: 'http://d21lz9b0v8r1zn.cloudfront.net/wp-content/uploads//2012/03/detail.jpg',
-			comment: 'New York blows up in this!'
-		},
-		{
-			title: 'Dark City',
-			image: 'https://i.chzbgr.com/full/5569379584/hA96709E0/',
-			comment: 'This looks mysterious. Cool!'
-		},
-		{
-			title: 'Hot Tub Time Machine',
-			image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG7vNmphIcVhEcybvSvMgbTkV6EE2twHBNanKvgDx3ZS7Ivn6Dtg',
-			comment: 'Someone said this was fun. Maybe!'
-		},
-		];
-	} else {
-		return JSON.parse(movies);
-	}
+    // Add a mock folder and define the data in the specific file, This will improve organization and make it easier to read
+    // Use ternary operator, simpler and shorter
+    if (!movies) {
+        return [
+            {
+                title: 'The Avengers',
+                image: 'http://d21lz9b0v8r1zn.cloudfront.net/wp-content/uploads//2012/03/detail.jpg',
+                comment: 'New York blows up in this!'
+            },
+            {
+                title: 'Dark City',
+                image: 'https://i.chzbgr.com/full/5569379584/hA96709E0/',
+                comment: 'This looks mysterious. Cool!'
+            },
+            {
+                title: 'Hot Tub Time Machine',
+                image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG7vNmphIcVhEcybvSvMgbTkV6EE2twHBNanKvgDx3ZS7Ivn6Dtg',
+                comment: 'Someone said this was fun. Maybe!'
+            },
+        ];
+    } else {
+        return JSON.parse(movies);
+    }
 }
 
+// Better to just pass the movie object into function, instead of passing "title, description, image".Reduces code and chance of mistake
 export function add(title, description, image) {
-	var movie = {};
-	movie.title = title;
-	movie.description = description;
-	movie.image = image;
+    // Avoid using var for defining variables, alternative keywords (let, const)
+    var movie = {};
 
-	var movies = getAllMovies();
-	movies.push(movie);
+    // Better to use more simpler, newer and readable syntax for creating the object
+    movie.title = title;
+    movie.description = description;
+    movie.image = image;
 
-	localStorage.setItem('movies-all', JSON.stringify(movies));
+    // Avoid using var for defining variables, alternative keywords (let, const)
+    var movies = getAllMovies();
+    movies.push(movie);
+    // If you have keys in localStorage and using them in different part of codes, that would be better to define variable for them
+    localStorage.setItem('movies-all', JSON.stringify(movies));
 
-	render();
+    // Strange way to mount the components
+    render();
 }
 
+// Better to just pass the movie object into function, instead of passing "title, description, image". Reduces code and chance of mistake
 export function addWatchedMovie(title, description, image) {
-	var movie = {};
-	movie.title = title;
-	movie.description = description;
-	movie.image = image;
-
-	var movies = getWatchedMovies();
-	movies.push(movie);
-
-	localStorage.setItem('movies-watched', JSON.stringify(movies));
-
-	render();
+    // Avoid using var for defining variables, alternative keywords (let, const)
+    var movie = {};
+    // Better to use more simpler, newer and readable syntax for creating the object
+    movie.title = title;
+    movie.description = description;
+    movie.image = image;
+    // Avoid using var for defining variables, alternative keywords (let, const)
+    var movies = getWatchedMovies();
+    movies.push(movie);
+    // If you have keys in localStorage and using them in different part of codes, that would be better to define variable for them
+    localStorage.setItem('movies-watched', JSON.stringify(movies));
+    // Strange way to mount the components
+    render();
 }
 
 export function removeWatchedMovie(title) {
-	var movies = getWatchedMovies();
+    // Avoid using var for defining variables, alternative keywords (let, const)
+    var movies = getWatchedMovies();
 
-	for (var i = 0; i < movies.length; i++) {
-	   if (!movies[i]) continue;
-		if (movies[i].title == title) {
-			movies[i] = null
-		}
-	}
-
-	localStorage.setItem('movies-watched', JSON.stringify(movies));
-
-	render();
+    // Its doing movies[i] = null, but it should really just remove the movie from the array.
+    // That is, we dont need to keep movie object in the array with "null" title.
+    // And we can probably just use newMovieSet = movies.filter(x => x.title !== title));
+    // Use === instead of == for strict comparison
+    for (var i = 0; i < movies.length; i++) {
+        if (!movies[i]) continue;
+        if (movies[i].title == title) {
+            movies[i] = null
+        }
+    }
+    // If you have keys in localStorage and using them in different part of codes, that would be better to define variable for them
+    localStorage.setItem('movies-watched', JSON.stringify(movies));
+    // Strange way to mount the components
+    render();
 }
 
+// Create components and pass props to them, let App element just renders the whole application
+// Would be better to upgrade react, as long as the app is not still that much big
 function render() {
-	ReactDOM.render(<App movies={getAllMovies()} watched={getWatchedMovies()} />, document.getElementById('root'))
+    ReactDOM.render(<App movies={getAllMovies()} watched={getWatchedMovies()} />, document.getElementById('root'))
 }
 
 render();
